24f4307e0645231db5b2406753c2b4cd2f3a345e
Denis <1676867@gmail.com>
2020-07-24 12:07:26 +0300

    first code

--- script.js
+++ script.js
@@ -1,8 +1,23 @@
-// "use strict";
+'use strict';
+
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
 
 
-// const result = confirm("Are you here?");
-// console.log(result);
+const a = prompt('Один из последних просмотренных фильмов?', ''),
+    b = prompt('На сколько оцените его?', ''),
+    c = prompt('Один из последних просмотренных фильмов?', ''),
+    d = prompt('На сколько оцените его?', '');
 
-const answer = prompt("Ваш есть 18?", "18");
-console.log(answer + 5);
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
